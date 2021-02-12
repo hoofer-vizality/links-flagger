505926b2853908ac775230820d531cdf7eb07f83
Hunter <60201971+HooferDevelops@users.noreply.github.com>
2021-02-11 22:17:48 -0500

    more changes

--- components/label.jsx
+++ components/label.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { FormItem, FormText, Tooltip, Button, Divider } from '@vizality/components/';
 
-module.exports = ({field,hoverText}) => (
+module.exports = ({field,hoverText,classType}) => (
    <Tooltip
-   text="This URL is potentially malicious. Proceed with caution."
+   text={hoverText}
    color="red"
-   className='vz-settings-button-item-button-wrapper suspicious-url-link'>
-    <div class="suspicious-url">
+   className='vz-settings-button-item-button-wrapper dangerous-url-link'>
+    <div class={`suspicious-url ${classType}`}>
         {field}
     </div>
    </Tooltip>
--- index.js
+++ index.js
@@ -7,40 +7,76 @@ import { getModule, getModuleByDisplayName, getModules } from '@vizality/webpack
 
 const label = require('./components/label.jsx');
 
-export default class HoofersFirstPlugin extends Plugin {
+const tooltipTypes = [
+    {
+        name: "Safe URL",
+        field_display: "No problems found!",
+        urlClass: "safe-url-link",
+        tooltipClass: "safe-url-tooltip-content"
+    },
+    {
+        name: "Unsafe URL",
+        field_display: "This potentially has unwanted content, proceed with caution.",
+        urlClass: "unsafe-url-link",
+        tooltipClass: "unsafe-url-tooltip-content"
+    },
+    {
+        name: "Dangerous URL",
+        field_display: "This URL was flagged as dangerous, proceed at your own risk.",
+        urlClass: "dangerous-url-link",
+        tooltipClass: "dangerous-url-tooltip-content"
+    }
+]
+
+export default class LinksFlagger extends Plugin {
 start () {
+    // theming
     this.injectStyles('style.scss');
+
+    // modules
     const MaskedLink = getModuleByDisplayName("MaskedLink", false)
     const Tooltip = getModuleByDisplayName('Tooltip', false)
-    const h = getModule(m => m.default?.displayName === 'Tooltip', false);
-    console.log(h);
-    console.log(Tooltip.prototype);
+
+    // injectors
     patch('tooltip-inject', Tooltip.prototype, "renderTooltip", (args, res) => {
         if (!res.props || !res.props.children || !res.props.targetElementRef || !res.props.targetElementRef.current)
             return;
         
-        if (res.props.targetElementRef.current.classList.contains("suspicious-url-link")){
-            res.props.tooltipClassName = "suspicious-url-tooltip-content"
-        }
-        
+        // probably bad way of doing it, don't care though
+        tooltipTypes.forEach(tooltipInfo=>{
+            console.log(res.props.targetElementRef.current.classList)
+            if (res.props.targetElementRef.current.classList.contains(tooltipInfo.urlClass)){
+                res.props.tooltipClassName = tooltipInfo.tooltipClass
+                res.props.children = tooltipInfo.field_display;
+            }
+        })
         
         return res;
     });
-    patch('title-inject', MaskedLink.prototype, "render", (args, res) => {
+    patch('link-inject', MaskedLink.prototype, "render", (args, res) => {
         if (!res.props || !res.props.children)
             return res;
         if (typeof(res.props.children) === "object")
             res.props.children = res.props.children[0];
+        
+        var customClass = "";
+
+
+        console.log(res.props.className)
+        if (res.props.className && res.props.className.includes("embedTitleLink"))
+            customClass = "link-title-container";
 
         if (!res.props.href.startsWith("https://discord"))
-            res.props.children = React.createElement(label, {field:res.props.children,hoverText:res.props.children});
+            res.props.children = React.createElement(label, {classType:customClass,field:res.props.children,hoverText:res.props.children});
         
         return res;
     });
+
+
   }
 
   stop () {
-     unpatch('title-inject')
      unpatch('tooltip-inject')
+     unpatch('link-inject')
   }
 }
